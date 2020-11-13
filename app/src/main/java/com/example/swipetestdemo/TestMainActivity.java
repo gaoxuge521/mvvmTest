edc42338e448c7c8d2c5da2b330acfa14b0b2c9b
gaoxuge <gaoxuge2020>
2020-11-13 21:42:27 +0800

    first commit

---
+++ app/src/main/java/com/example/swipetestdemo/TestMainActivity.java
@@ -0,0 +1,21 @@
+package com.example.swipetestdemo;
+
+import android.os.Bundle;
+import com.example.swipetestdemo.databinding.ActivityTestMainBinding;
+import me.goldze.mvvmhabit.base.BaseActivity;
+
+
+public class TestMainActivity extends BaseActivity {
+
+
+
+    @Override
+    public int initContentView(Bundle savedInstanceState) {
+        return R.layout.activity_test_main;
+    }
+
+    @Override
+    public int initVariableId() {
+        return BR.viewModel;
+    }
+}
---
+++ app/src/main/java/com/example/swipetestdemo/TestMainViewModel.java
@@ -0,0 +1,11 @@
+package com.example.swipetestdemo;
+
+import android.app.Application;
+
+import me.goldze.mvvmhabit.base.BaseViewModel;
+
+public class TestMainViewModel extends BaseViewModel {
+    public TestMainViewModel(Application application) {
+        super(application);
+    }
+}
